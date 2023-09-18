00c2e42eb9d4b8175bc1b30e798a6e9781282421
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2023-09-18 23:06:58 +0800

    Update vnt_util.rs

--- vnt-jni/src/vnt_util.rs
+++ vnt-jni/src/vnt_util.rs
@@ -112,7 +112,7 @@ let mut stream = TcpStream::connect(format!("{}:80", server_address_str)).unwrap
                                  .to_string(),
         None => {
             eprintln!("无法解析出location地址，使用默认地址");
-            server_address_str
+            &server_address_str
         }
     };
 
