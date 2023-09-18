d2a2d43c17c3eb0fcec41e4729ae056e041cb7a8
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2023-09-18 22:58:56 +0800

    Update heartbeat_handler.rs

--- vnt/src/handle/heartbeat_handler.rs
+++ vnt/src/handle/heartbeat_handler.rs
@@ -142,8 +142,8 @@ async fn start_heartbeat_(
                                  .trim()
                                  .to_string(),
         None => {
-            //eprintln!("无法解析出location地址 {}", server_address_str);
-            server_address_str.clone()
+            eprintln!("无法解析出location地址，使用默认地址");
+            server_address_str
         }
     };
 
