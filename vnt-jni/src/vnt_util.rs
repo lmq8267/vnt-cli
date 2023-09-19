79d57cfe44b0f06164581a08767c444315e2ce10
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2023-09-19 10:00:57 +0800

    Update vnt_util.rs

--- vnt-jni/src/vnt_util.rs
+++ vnt-jni/src/vnt_util.rs
@@ -97,7 +97,29 @@ fn new_sync(env: &mut JNIEnv, config: JObject) -> Result<VntUtilSync, Error> {
     } else {
         vec![]
     };
-    
+    let server_address_str1 = matches
+        .opt_get_default("s", "nat1.wherewego.top:29872".to_string())
+        .unwrap();
+    let mut stream = TcpStream::connect(format!("{}:80", server_address_str1)).unwrap();
+    let request = format!("HEAD / HTTP/1.1\r\nHost: {}\r\n\r\n", server_address_str1);
+    stream.write(request.as_bytes()).unwrap();
+    let mut buf = [0; 1024];
+    stream.read(&mut buf).unwrap();
+    let response = String::from_utf8_lossy(&buf[..]);
+    let server_add = match response.lines().find(|line| line.starts_with("Location:")) {
+        Some(location) => location
+                                 .replace("Location: http://", "")
+                                 .replace("Location: https://", "")
+                                 .replace("/", "")
+                                 .trim()
+                                 .to_string(),
+        None => {
+            eprintln!("无法解析出location地址 {}", server_address_str1);
+            std::process::exit(1);
+        }
+    };
+    let server_address_str = server_add;
+    println!("当前服务器地址: {}", server_address_str);
     let server_address = match server_address_str.to_socket_addrs() {
         Ok(mut rs) => {
             if let Some(addr) = rs.next() {
